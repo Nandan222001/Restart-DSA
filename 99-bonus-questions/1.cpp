6272146eb790cb360ab328c11d5e7b4e34a1977d
Nandan222001 <nandankumar222001@gmail.com>
2025-08-12 19:38:17 +0530

    Changes

---
+++ 99-bonus-questions/1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Your code here
+    int n;
+    if(cin>>n){
+        cout<<"Nandan is Here"<<endl;
+    }
+    return 0;
+}
---
+++ 99-bonus-questions/2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Your code here
+    int n;
+    if(cout<<n){
+        cout<<"Nandan is Here"<<endl;
+    }
+    return 0;
+}
